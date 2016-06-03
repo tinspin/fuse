85071271f3a8a05b5d9c069daa5b4e0f925cd6b3
Marc Larue <marc@rupy.se>
2016-06-03 10:15:13 +0200

    C++

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-"C:\Program Files (x86)\Mono\bin\mcs.bat" src\Fuse.cs
+:: C++ with MinGW
+g++ src\Fuse.cpp -o src\Fuse.exe -lws2_32
+:: C# with Mono
+::"C:\Program Files (x86)\Mono\bin\mcs.bat" src\Fuse.cs
 pause
