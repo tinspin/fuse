4ab92d92624e1b37e0f7b2735b36fc15c2f4aeda
Marc Larue <marc@rupy.se>
2016-06-04 21:55:43 +0200

    C++

--- src/Fuse.cpp
+++ src/Fuse.cpp
@@ -8,12 +8,14 @@
 # include <sys/socket.h>
 #endif
 
+using namespace std;
+
 /*
- * This uses two threads, one for each socket.
+ * This uses two threads, one for each socket/queue.
  * The buffer stream is used for the chunked incoming data.
  */
 
-class BufferInputStream : public std::basic_streambuf<char>
+class BufferInputStream : public basic_streambuf<char>
 {
 private:
 	static const int SIZE = 128;
@@ -25,24 +27,23 @@ public:
 	~BufferInputStream() {}
 	
 protected:
-	int overflow(int_type c)
-	{
-		return c;
-	}
-
-	int sync()
-	{
-		return 0;
-	}
-
+	int overflow(int c) { return c; }
+	int sync() { return 0; }
 	int underflow()
 	{
 		if(gptr() < egptr())
 			return *gptr();
 
-		int num;
-		if((num = recv(sock, reinterpret_cast<char*>(ibuf), SIZE, 0)) <= 0)
+#ifdef __WIN32__
+		int num = recv(sock, reinterpret_cast<char*>(ibuf), SIZE, 0);
+#else
+		int num = read(sock, reinterpret_cast<char*>(ibuf), SIZE);
+#endif
+
+		if(num <= 0)
 			return EOF;
+
+		cout << "IN: " << num << endl;
 
 		setg(ibuf, ibuf, ibuf + num);
 		return *gptr();
@@ -54,34 +55,40 @@ BufferInputStream::BufferInputStream(int sock) {
 	setg(ibuf, ibuf, ibuf);
 }
 
-void *PrintHello(void *threadid)
+void *Pull(void *threadid)
 {
 	long tid = (long) threadid;
-	std::cout << "Hello World! Thread ID, " << tid << std::endl;
+	cout << "THREAD: " << tid << endl;
 	pthread_exit(NULL);
 }
 
 main()
 {
 #ifdef __WIN32__
-	WORD versionWanted = MAKEWORD(1, 1);
+	WORD versionWanted = MAKEWORD(2, 2);
 	WSADATA wsaData;
 	WSAStartup(versionWanted, &wsaData);
 #endif
 
 	pthread_t thread;
-	pthread_create(&thread, NULL, PrintHello, (void *) 0);
+	pthread_create(&thread, NULL, Pull, (void *) 0);
 
-	char data[100] = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+	char data[100] = "GET /pull HTTP/1.1\r\nHost: bitcoinbankbook.com\r\n\r\n";
 
+	struct hostent *host = gethostbyname("bitcoinbankbook.com");
 	struct sockaddr_in address;
+	
+	int flag = 1;
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-
+	setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (char*) &flag, sizeof(int));
+	
 	address.sin_family = AF_INET;
-	address.sin_port = htons(8000);
-	address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	address.sin_port = htons(80);
+	address.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)(host -> h_addr_list[0]))); // inet_addr("127.0.0.1");
 
-	connect(sock, (struct sockaddr *) &address, sizeof (address));
+	cout << inet_ntoa(*(struct in_addr*)(host -> h_addr_list[0])) << endl;
+
+	connect(sock, (struct sockaddr*) &address, sizeof (address));
 	
 	int result = 0;
 	
@@ -91,16 +98,22 @@ main()
 	result = write(sock, data, strlen(data));
 #endif
 
-	printf("out: %d\n", result);
+	printf("OUT: %d\n", result);
 	
-	BufferInputStream input(sock);
+	BufferInputStream buffer(sock);
+	istream stream(&buffer);
+		
+	string line;
 	
-	std::string line;
-	std::istream stream(&input);
-	std::getline(stream, line);
+	for(int i = 0; i < 50; i++) {
+		getline(stream, line, '\r');
+		cout << line << flush;
+	}
 	
-	std::cout << line << std::endl;
-	//printf("in: %d\n", line);
+	//char c;
+	//stream.get(c);
+	//printf("in: %d\n", c);
+	//printf("in: %d\n", 'H');
 /*
 	char buffer[1024];
 	
@@ -119,6 +132,5 @@ main()
 	close(sock);
 #endif
 	
-	std::cout << "Hello World!";
 	return 0;
 }
