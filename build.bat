ab284e9bfa2cd4d43fd60856bab2ef1cef0cf1de
Marc Larue <marc@rupy.se>
2016-06-04 22:26:41 +0200

    C++

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-:: C++ with MinGW
+:: C++ with MinGW, you might have to copy libstdc++-6.dll to the src folder!
 g++ src\Fuse.cpp -o src\Fuse.exe -lws2_32 -lpthread -DHAVE_STRUCT_TIMESPEC
 :: C# with Mono
 ::"C:\Program Files (x86)\Mono\bin\mcs.bat" src\Fuse.cs
