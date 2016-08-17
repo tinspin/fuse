ab88380831025f76e8821d718ae8278388c95a01
Marc Larue <marc@rupy.se>
2016-08-17 08:01:42 +0200

    C++

--- src/Fuse.cpp
+++ src/Fuse.cpp
@@ -81,7 +81,7 @@ protected:
     uint32 m_h[8];
 };
  
-std::string sha256(std::string input);
+string sha256(string input);
  
 #define SHA2_SHFR(x, n)    (x >> n)
 #define SHA2_ROTR(x, n)   ((x >> n) | (x << ((sizeof(x) << 3) - n)))
